1d69676f511665fba1939687140d27eb76377e76
karankumarshreds <naileditnerd@gmail.com>
2021-07-17 17:22:49 +0530

    add info tag widget

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,48 @@ class MyApp extends StatelessWidget {
                     'Karan Kumar',
                     style: TextStyle(
                       fontSize: 50,
-                      fontWeight: FontWeight.bold,
+                      // fontWeight: FontWeight.bold,
                       fontFamily: 'Pacifico',
                       color: Colors.white
                     ),
+                  ),
+                  Text(
+                      'FLUTTER DEVELOPER',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500
+                      )
+                  ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 30
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                              Icons.phone,
+                              color: Colors.teal.shade900
+                          ),
+                          // to add space between other children
+                          SizedBox(
+                            width: 50.0,
+                          ),
+                          Text(
+
+                              '+91 704-287-1235',
+                              style: TextStyle(
+                                  color: Colors.teal.shade900,
+                                  fontFamily: 'Source San Pro',
+                                  fontSize: 20
+                              ),
+                          )
+                        ]
+                    )
                   )
                 ]
             )
